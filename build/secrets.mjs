8b82e95d9d804a097e64a1b7a67a3a4c2d8d4548
Remy Suen <remy.suen@docker.com>
2025-10-10 17:51:37 -0400

    Explicitly create a String object to workaround TS2367
    Signed-off-by: Remy Suen <remy.suen@docker.com>

--- build/secrets.mjs
+++ build/secrets.mjs
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 const content = `export const SECRETS = {
-  TELEMETRY_HOST: '${process.env.TELEMETRY_HOST === undefined ? '' : process.env.TELEMETRY_HOST}',
-  TELEMETRY_PATH: '${process.env.TELEMETRY_PATH === undefined ? '' : process.env.TELEMETRY_PATH}',
-  TELEMETRY_KEY: '${process.env.TELEMETRY_KEY === undefined ? '' : process.env.TELEMETRY_KEY}',
-  BUGSNAG_API_KEY: '${process.env.BUGSNAG_API_KEY === undefined ? '' : process.env.BUGSNAG_API_KEY}',
+  // explicitly create a String to workaround TS2367
+  TELEMETRY_HOST: String('${process.env.TELEMETRY_HOST === undefined ? '' : process.env.TELEMETRY_HOST}'),
+  TELEMETRY_PATH: String('${process.env.TELEMETRY_PATH === undefined ? '' : process.env.TELEMETRY_PATH}'),
+  TELEMETRY_KEY: String('${process.env.TELEMETRY_KEY === undefined ? '' : process.env.TELEMETRY_KEY}'),
+  BUGSNAG_API_KEY: String('${process.env.BUGSNAG_API_KEY === undefined ? '' : process.env.BUGSNAG_API_KEY}'),
 } as const;
 `;
 fs.writeFileSync('src/secrets.ts', content);
